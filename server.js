bc2f391635d5b4fd1ff3b179066975efb5a84788
Tambet Masik <tambet@grabcad.com>
2015-08-28 12:53:36 +0300

    Set default workers to 10

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var prerender = require('./lib');
 process.env.PORT = process.env.PORT || 4000
 
 var server = prerender({
-    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
+    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS || 10,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
     messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
